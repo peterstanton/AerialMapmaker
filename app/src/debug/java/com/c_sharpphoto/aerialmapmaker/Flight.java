3134919e4a2a16bcb2fbaf8a8fbc4e9d99c9e489
shipdriver <tribunus@msn.com>
2016-04-20 13:40:39 -0700

    Added primary activities

---
+++ app/src/debug/java/com/c_sharpphoto/aerialmapmaker/Flight.java
@@ -0,0 +1,13 @@
+package com.c_sharpphoto.aerialmapmaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Flight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight);
+    }
+}
